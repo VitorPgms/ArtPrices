e405fcf42e5380ff7be49ef2545d7d886997a602
Vitor de Paula Gomes <112875256+VitorPgms@users.noreply.github.com>
2025-02-27 22:08:00 -0300

    Adicionando um Spinner no peso da resina

--- app/src/main/java/com/example/artprice/MainActivity.kt
+++ app/src/main/java/com/example/artprice/MainActivity.kt
@@ -22,16 +22,24 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val spinner = binding.spiTipo
 
-        //Criando Adapter para o Spinner
+        val spinnerPesoResina = binding.spiResina
+        val spinnerProfit = binding.spiTipo
+
+        //Criando Adapter para o SpinnerPesoResina
+        val pesoResinaArray = resources.getStringArray(R.array.peso_resina)
+        val adapterPesoResina = ArrayAdapter(this, android.R.layout.simple_spinner_item, pesoResinaArray)
+        adapterPesoResina.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinnerPesoResina.adapter = adapterPesoResina
+
+        //Criando Adapter para o SpinnerProfit
         val option = resources.getStringArray(R.array.opcao_calculo)
         val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, option)
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        spinner.adapter = adapter
+        spinnerProfit.adapter = adapter
 
         binding.btnCalcular.setOnClickListener {
-            val resina = binding.editResina.text.toString()
+            val resina = binding.spiResina.toString()
             val value = binding.editValor.text.toString()
             val pieceWeight = binding.editPeso.text.toString()
             val profit = binding.editLucro.text.toString()
@@ -50,22 +58,29 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun calculationPrice(){
-        val resinaId = binding.editResina
         val valueId = binding.editValor
         val weightId = binding.editPeso
         val accessoryId = binding.editAcessorio
         val optionSelect = binding.spiTipo.selectedItem.toString()
+        val spiPesoResina = binding.spiResina.selectedItem.toString()
         val profitId = binding.editLucro
         val feesId = binding.editTaxa
 
-        val resina = resinaId.text.toString().toDoubleOrNull() ?: 0.0
+        val pesoResina = when {
+            spiPesoResina.contains("kg") -> spiPesoResina.replace("kg", "").trim().replace(",", ".").toDoubleOrNull()?.times(1000)
+            spiPesoResina.contains("g") -> spiPesoResina.replace("g", "").trim().toDoubleOrNull()
+            else -> 0.0
+        } ?: 0.0
+
+
+        val resinaID = binding.spiResina.selectedItem.toString().toDoubleOrNull()
         val value = valueId.text.toString().toDoubleOrNull() ?: 1.0
         val weight = weightId.text.toString().toDoubleOrNull() ?: 0.0
         val accessory = accessoryId.text.toString().toDoubleOrNull() ?: 0.0
         val profit = profitId.text.toString().toDoubleOrNull() ?: 0.0
         val fees = feesId.text.toString().toDoubleOrNull() ?: 0.0
 
-        val pricePiece = (value / resina) * weight
+        val pricePiece = (value / pesoResina) * weight
         val costPiece = pricePiece + accessory
 
         val spinnerResult = when(optionSelect) {
